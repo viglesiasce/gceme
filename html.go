310892970c1c945c2a0c33175008ef955eff2e97
Vic Iglesias <viglesias@google.com>
2017-05-19 23:10:14 -0700

    Update html.go

--- html.go
+++ html.go
@@ -11,7 +11,7 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License.zxcvzxcvzxcvz
+# limitations under the License.qwer23
 **/
 
 package main
